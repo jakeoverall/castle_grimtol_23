3acf50e6b055704c680d5b60df699ed354a2a09e
Jake Overall <joverall22@gmail.com>
2023-10-29 10:38:19 -0600

    somewhat working

--- scripts/player/player_base.gd
+++ scripts/player/player_base.gd
@@ -1,11 +1,22 @@
 extends Node
 
+@export_category('Player Config')
+@export var heath : float = 10
+@export var speed : float = 100
+@export var exp : int = 0
+@export var gold : int = 0
 
-# Called when the node enters the scene tree for the first time.
+@export_category('Player Refs')
+@export var player_movement_ref : CharacterMovement
+@export var health_ref : Health
+
+signal player_death()
+
 func _ready():
-	pass # Replace with function body.
+	player_movement_ref.speed = speed
+	health_ref.health = heath
+	health_ref.health_expired.connect(_on_player_death)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_player_death():
+	get_tree().reload_current_scene()
